d70878dcfd0976cfb196d21f8b663ca945ec76c8
Josh Carreer <josh.carreer@gmail.com>
2013-10-21 09:18:26 -0400

    Created CriminalIntent App, crimefragment class,crime class and fragment_crime.xml and wired up the text editor widget

---
+++ homework5/CriminalIntent/src/com/joshcarreer/android/criminalintent/Crime.java
@@ -0,0 +1,21 @@
+package com.joshcarreer.android.criminalintent;
+
+import java.util.UUID;
+
+public class Crime {
+	private UUID mId;
+	private String mTitle;
+	public Crime() {
+		setmId(UUID.randomUUID());
+	}
+	public String getmTitle() {
+		return mTitle;
+	}
+	public void setmTitle(String mTitle) {
+		this.mTitle = mTitle;
+	}
+	public UUID getmId() {
+		return mId;
+	}
+	
+}
---
+++ homework5/CriminalIntent/src/com/joshcarreer/android/criminalintent/CrimeActivity.java
@@ -0,0 +1,16 @@
+package com.joshcarreer.android.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+
+public class CrimeActivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_crime);
+	}
+
+
+}
