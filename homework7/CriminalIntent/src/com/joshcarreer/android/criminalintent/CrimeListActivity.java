d01df4c08fa2852dd0aefe4e71791d68df3c37bf
Josh Carreer <josh.carreer@gmail.com>
2013-10-30 09:43:19 -0400

    updating

---
+++ homework7/CriminalIntent/src/com/joshcarreer/android/criminalintent/CrimeListActivity.java
@@ -0,0 +1,13 @@
+package com.joshcarreer.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+	@Override
+	protected Fragment createFragment() {
+		return new CrimeListFragment();
+		
+	}
+
+}
